547d24747ff01842f250ac2c735607fac0bcb7f4
pokabu55 <okazumaru@gmail.com>
2021-04-04 00:02:59 +0900

    基本構成の登録

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
