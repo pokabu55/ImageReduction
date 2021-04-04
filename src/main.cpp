9be02afa6198b9bfc4f378e46a92f9b57ea5cc2c
pokabu55 <okazumaru@gmail.com>
2021-04-04 18:06:32 +0900

    指定ディレクトリ内のファイルリストの作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h>     // getopt
+#include <sys/stat.h>   // stat
+#include <vector>
+
+
+// ディレクトリから取得
+#include <sys/types.h>
+//#include <cstdlib>
+#include <dirent.h>
+//#include <sys/stat.h>
+//#include <algorithm>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
-bool parseOption(int argc, char *argv[], std::string directoryName, double *ratio)
+bool parseOption(int argc, char *argv[], std::string *directoryName, double *ratio)
 {
     int i, opt;
     char *dirName = nullptr;
@@ -23,7 +33,8 @@ bool parseOption(int argc, char *argv[], std::string directoryName, double *rati
                 // ディレクトリ名指定のオプション
                 dirName = optarg;
                 //printf("-d がオプションとして渡されました\n");
-                printf("dirName = %s\n", dirName);
+                //printf("dirName = %s\n", dirName);
+                *directoryName = dirName;
                 break;
 
 // TODO: まだ使わないオプション
@@ -57,7 +68,68 @@ bool parseOption(int argc, char *argv[], std::string directoryName, double *rati
     return true;
 }
 
-bool makeResultDirectory(std::string srcDir, std::string dstDir)
+bool makeResultDirectory(std::string srcDir, std::string &dstDir, std::string suffix)
+{
+    struct stat statBuf;
+
+    // src の存在確認
+    if (stat(srcDir.c_str(), &statBuf) != 0) {
+        std::cout << "srcDir :" << srcDir << " is not found." << std::endl;
+        return false;
+    }
+
+    // dst の命名
+    dstDir = srcDir + suffix;
+
+    // dst 作成
+    if (mkdir(dstDir.c_str(), 0775) !=0) {
+        std::cout << "Failed to make detDir :" << dstDir << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool getFileList(std::string path, std::vector<std::string> &fileList, std::string extension)
+{
+	std::string fname;
+	std::vector<std::string> cand;
+
+	// ディレクトリへのポインタ
+	DIR *dp;
+
+	 // readdir() で返されるエントリーポイント
+	dirent* entry;
+
+	dp = opendir(path.c_str());
+	if (dp==NULL) return false;
+
+	do {
+		// エントリーポイント
+		entry = readdir(dp);
+
+		if (entry != NULL) {
+			fname = entry->d_name;
+
+			// ファイルの場合
+			auto pos1 = fname.find(extension);
+			if (pos1 == std::string::npos) continue;
+
+			fileList.push_back(path+fname);
+		}
+
+	} while (entry != NULL);
+
+	closedir(dp);
+
+	if (fileList.size() <= 0) return false;
+
+	// sort
+	std::sort(fileList.begin(), fileList.end());
+	return true;
+}
+
+bool createResizeImage(std::vector<std::string> fileList, std::string dirName)
 {
     return true;
 }
@@ -71,18 +143,21 @@ int main (int argc, char *argv[])
     std::string dirNameDst = "";
 
     // オプション解析
-    if (!parseOption(argc, argv, dirNameSrc &ratio)) return -1;
+    if (!parseOption(argc, argv, &dirNameSrc, &ratio)) return -1;
 
     // ディレクトリの確認
-    std::cout << dirNameSrc << std::endl;
-
+    //std::cout << "src dir name = " << dirNameSrc << std::endl;
 
     // 同じ階層に結果ディレクトリの作成
-    if (!makeResultDirectory(dirNameSrc, dirNameDst))
+    std::string suffix = "_resize";
+    if (!makeResultDirectory(dirNameSrc, dirNameDst, suffix)) return -1;
 
     // ディレクトリ内のすべてのjpgのファイル名をリスト化
+    std::vector<std::string> fileList;
+    if (!getFileList(dirNameSrc, fileList, "jpg")) return -1;
 
     // 縮小処理して保存
+    if (!createResizeImage(fileList, dirNameDst)) return -1;
 
     // このとき、メタ情報も残したいのだができる？
 
